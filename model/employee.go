dbe547e1b06ce1e65c81133674e3d2e4da808021
sidz111 <sssurwade2212@gmail.com>
2026-02-22 12:14:59 +0530

    Model struct created

--- model/employee.go
+++ model/employee.go
@@ -1,1 +1,7 @@
 package model
+
+type Employee struct {
+	Name    string  `json:"name" binding:"required"`
+	Address string  `json:"address" binding:"required"`
+	Salary  float64 `json:"salary" binding:"required"`
+}
